a98558dabc440f8678adf45c27966a5e65ac5e96
Taylor Allen <50384820+taylor-allen@users.noreply.github.com>
2024-04-21 01:48:23 +0000

    final draft

--- src/models.py
+++ src/models.py
@@ -1,32 +1,58 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Date
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from datetime import date
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    title = Column(String(250), nullable=False)
+    photo_or_video = Column(String(250), nullable=False)
+    description = Column(String(250))
+    likes = Column(Integer, default=0)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-    def to_dict(self):
-        return {}
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    text = Column(String(250), nullable=False)
+    likes = Column(Integer, default=0)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Message(Base):
+    __tablename__ = 'message'
+    id = Column(Integer, primary_key=True)
+    text = Column(String(250), nullable=False)
+    create_date = Column(Date, default=date.today(), nullable=False)
+    sender_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    sender = relationship(User)
+    rep_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    rep = relationship(User)
+
+class Stories(Base):
+    __tablename__ = 'stories'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250))
+    photo_or_video = Column(String(250), nullable=False)
+    description = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship(User)
 
 ## Draw from SQLAlchemy base
 try:
